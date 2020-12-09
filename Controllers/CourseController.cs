c7b73bcc81f37bcaab10ef9ea96a863ff5ba0443
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-10 00:52:44 +0300

    Controller Eklendi

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Controllers
+{
+    //localhost:5000/course
+    public class CourseController:Controller
+    {
+        //action methods
+
+        //localhost:5000/course/index
+        public string Index()
+        {
+            return "course/index";
+        }
+
+        //localhost:5000/course/list
+        public string List()
+        {
+            return "course/list";
+        }
+
+    }
+}
